682e2cd45834c4a8c5e6912cd40a480110f7e001
aymen96tlili-cloud <aymen96tlili@gmail.com>
2025-12-07 21:48:03 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,8 @@ function App() {
     const fetchProfile = async () => {
       const { data, error } = await supabase
         .from("users")
-        .select("id, full_name, part_id, shift_id")
-        .eq("auth_user_id", "ef369cad-3ba6-49e2-aeae-1c2c63ce6bcf");
+        .select("id, full_name, part_id, shift_id, role")
+        .eq("auth_user_id", "ef369cad-39b2-4eae-eaae-1c263c4d2f2b");
 
       if (error) {
         console.error("Supabase error:", error);
@@ -34,6 +34,7 @@ function App() {
           <p><strong>Name:</strong> {profile.full_name}</p>
           <p><strong>Part:</strong> {profile.part_id}</p>
           <p><strong>Shift:</strong> {profile.shift_id}</p>
+          <p><strong>Role:</strong> {profile.role}</p>
         </div>
       ) : (
         <p>No profile found.</p>
