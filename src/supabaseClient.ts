d312dd82560ed790c7eec09c026aa05c6d4abba6
aymen96tlili-cloud <aymen96tlili@gmail.com>
2025-12-07 17:08:49 +0100

    Update Supabase anon key in supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://zjxedlarvkccijspnwwo.supabase.co"
-const supabaseAnonKey = "sb_publishable_51jcxIYbafMbl3GB9-4v7A_NQD_cK3C"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpqeGVkbGFydmtjY2lqc3Bud3dvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ3OTkxNTcsImV4cCI6MjA4MDM3NTE1N30.46g4qCvPidBttY4swjAiE9qtATxkBlqp0tkXYXWWWwk"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
